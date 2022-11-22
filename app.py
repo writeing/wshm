375eea61dda43b81cc6cf9e8aabf6f9fd12315e4
writeing_home <xuanchaowang@qq.com>
2022-11-22 22:36:10 +0800

    has finish

--- app.py
+++ app.py
@@ -1,38 +1,156 @@
 from flask import Flask,render_template,redirect,request
 from flask import url_for
 from markupsafe import escape
-import requests
 import os
+import re
+from spider import spider
+
 app = Flask(__name__)
 
 
 movies = {}
 
-def readLocalFile():
-    titlePath = 'static/title'
-    titleFileNames = os.listdir(titlePath)
-    dpicPath = 'static/dpic'
-    dpicFileNames = os.listdir(dpicPath)
-    movies['title'] = titleFileNames
-    movies['dpic'] = dpicFileNames
 
-@app.route('/')
-def hello():
-    readLocalFile()
-    return render_template('index.html',name = 'wshm',movies=movies)
+def readLocalFile(direct = 'week'):
+    global movies
+    movies = {}
+    titlePath = 'static/title/' + direct
+    try:
+        titleFileNames = os.listdir(titlePath)
+    except:
+        downLoadcmd(2,direct)
+        titleFileNames = os.listdir(titlePath)
+    dpicPath = 'static/dpic/'+ direct
+    try:
+        dpicFileNames = os.listdir(dpicPath)
+    except:
+        downLoadcmd(1,direct)
+        dpicFileNames = os.listdir(dpicPath)                
+    for names in titleFileNames:
+        name = re.split('\.',names)[0]
+        movies[name] = name + ".png"
+sg_items = []
+def readLocalTitle(direct,name):
+    global sg_items    
+    with open('static/title/'+ direct +'/' + name + '.ini','r') as file:
+        sg_items = file.readlines()
+
+def sort_key(s):
+    # 排序关键字匹配
+    # 匹配开头数字序号
+    if s:
+        try:
+            c = re.findall('^\d+', s)[0]
+        except:
+            c = -1
+        return int(c)
+
+def strsort(alist):
+    alist.sort(key=sort_key)
+    return alist
+
+imgList = []
+def readLocalImg(direct,name,num):
+    global imgList
+    path = 'static/images/' + direct + '/'+ name + "/" + num
+    imgList = strsort(os.listdir(path))
+    print(imgList)
+
+homeURL= 'https://www.wshm23.com/'
+
+def downLoadItem(item,direct='week'):
+    thread1 = spider(homeURL,update = False,itemNames = {item:0},isSaveHtml=False,direct=direct)
+    thread1.start()
+    thread1.join(1)
+    
+def downLoadcmd(cmd,direct='week'):
+    thread1 = spider(homeURL,update = False,isSaveHtml=False,cmd=cmd,direct=direct)
+    thread1.start()
+    thread1.join(1)
+globalUpdate = False
+def downLoadInit():
+    global globalUpdate
+    thread1 = spider(homeURL,update = globalUpdate,isSaveHtml=True,cmd = 3)
+    thread1.start()
+    thread1.join(1)
+    return thread1.yearNameList
+
+
+historyList = []
+
+def buttonExec(request):
+    global historyList
+    if request.method == 'POST': 
+        bt_dpic = request.values.get('dpic')
+        bt_title = request.values.get('title')
+        bt_root = request.values.get('root')
+        print(bt_dpic)
+        print(bt_title)
+        print(bt_root)
+        for year in historyList:
+            bt_year = request.values.get(year)
+            if bt_year == year:
+                print(bt_year)
+                return year
+        print(bt_dpic)
+        print(bt_title)
+        print(bt_root)
+        
+        if (bt_dpic == '更新dpic'):
+            downLoadcmd(1,'week')
+        if (bt_title == '更新title'):
+            downLoadcmd(2,'week')
+        if (bt_root == '更新主页面'):
+            downLoadInit()
+            
+    return 'week'
+@app.route('/',methods=['POST', 'GET'])
+def rootHome():
+    global historyList
+    if len(historyList) == 0:
+        historyList = downLoadInit()
+    value = buttonExec(request)    
+    readLocalFile(value)
+    return render_template('index.html',name = 'wshm',movies=movies,historyList = historyList,direct =value )
 
 @app.route('/user/<name>')
 def user_page(name):
     return f'User: {escape(name)}'
 
+@app.route('/wshm/up/<direct>/<item>/<int:num>')
+def page_up(direct,item,num):
+    num = num - 1
+    num = str(num)
+    readLocalImg(direct,item,num)
+    return render_template('showImg.html',name = item,num = num,imgList=imgList,direct=direct)
+
+@app.route('/wshm/down/<direct>/<item>/<int:num>')
+def page_down(direct,item,num):
+    num = num + 1
+    num = str(num)
+    readLocalImg(direct,item,num)
+    return render_template('showImg.html',name = item,num = num,imgList=imgList,direct=direct)
+
+@app.route('/wshm/<direct>/<item>')
+def wshmItem(direct,item):
+    readLocalTitle(direct,item)
+    downLoadItem(item,direct)
+    return render_template('item.html',name = 'wshm',allItems = sg_items,item = item,direct=direct)
+
+@app.route('/wshm/img/<direct>/<item>/<num>/<itemName>')
+def wshmImgShow(direct,item,num,itemName):
+    readLocalImg(direct,item,num)
+    return render_template('showImg.html',direct = direct,name = item,num = num,imgList=imgList,itemName=itemName)
+
+
 @app.route('/wshm/main')
 def wshm_main():
-    return redirect(url_for(hello))
+    return redirect(url_for(test_url_for))
 
 @app.route('/test')
 def test_url_for():
     # 下面是一些调用示例（请访问 http://localhost:5000/test 后在命令行窗口查看输出的 URL）：
-    print(url_for('hello'))  # 生成 hello 视图函数对应的 URL，将会输出：/
+    print(url_for('rootHome'))  # 生成 hello 视图函数对应的 URL，将会输出：/
     # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
     print(url_for('user_page', name='greyli'))  # 输出：/user/greyli
     print(url_for('user_page', name='peter'))  # 输出：/user/peter
@@ -40,3 +158,9 @@ def test_url_for():
     # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
     return 'Test page'
+
+if __name__ == '__main__':
+    app.run(debug = True)
+    globalUpdate = False
+
+
--- spider.py
+++ spider.py
@@ -6,7 +6,7 @@ import time
 from loguru import logger as log
 import threading
 import sys
-
+from concurrent.futures import ThreadPoolExecutor
 
 class spider(threading.Thread):
     homeUrl = 'https://www.wshm23.com/'
@@ -14,34 +14,41 @@ class spider(threading.Thread):
     index = 0
     basePathUrl = 'https://img.pic-server.com/'
     serviceIndex = 0
-    def __init__(self,fileUrl,update= False,itemNames={},isSaveHtml = False):
+    
+    def __init__(self,fileUrl,update= False,itemNames={},isSaveHtml = False,cmd=0,direct='week'):
         threading.Thread.__init__(self)
         self.homeUrl = fileUrl
         self.update = update
         self.updateItemNames = itemNames
         self.isSaveHtml = isSaveHtml
+        self.cmd = cmd
+        self.direct = direct
+        self.homeHtml = self.downHtml()
+        self.getWeekHref()
+        self.foreachWeekHTML()
+        self.foreachYearHTML()
+        
     def downHtml(self,url = homeUrl,path = 'temp/',filename = 'home.html'):
         html = ''
-
         if os.path.exists(path + filename) and not self.update:
             with open(path + filename,'r') as ff:
                 html = ff.read()
-            log.debug('had down html\n')
+            log.debug('had down html {0}\n',filename)
         else:
             html = requests.get(url).text
             if self.isSaveHtml:
                 with open(path + filename,'w') as ff:
                     ff.write(html)
-            log.debug('down now html\n')
+            log.debug('down now html {0}\n',filename)
         return html
     def downImage(self,url,name,path,imgType = '.png'):
-        if os.path.exists("static/" + path + name + imgType):
+        if os.path.exists( path + name + imgType):
             log.info('image:{0} had down \n',name)
             return 200
         img = requests.get(url)
         if img.status_code != 200:
             return img.status_code
-        with open("static/" + path +  name + imgType,'wb') as file:
+        with open(path +  name + imgType,'wb') as file:
              file.write(img.content)
         return img.status_code
     
@@ -52,7 +59,7 @@ class spider(threading.Thread):
 
     #https://img.pic-server.com/[0]/2022-11-14/717/139/1.jpg
     def getNowItemLink(self,index):
-        itemHtml = self.downHtml(self.homeUrl + self.imgItemLinkList[index],'temp/',self.itemname +"_" + str(index + 1) +"_item.html")
+        itemHtml = self.downHtml(self.homeUrl + self.imgItemLinkList[index],'temp/',str(index + 1) +"_item.html")
         itemHtml = etree.HTML(itemHtml)
         try:
             jpgOriginUrl = itemHtml.xpath('//div[@class="playerall"]/img/@src')[0]
@@ -64,9 +71,8 @@ class spider(threading.Thread):
         log.info(jpgModuleUrl)
         return jpgModuleUrl
 
-    def combinationImageUrl(self,index,update = True):
+    def combinationImageUrl(self,index,path,update = True):
         self.curItem = index - 1
-
         if update == False or self.serviceIndex == 0:
             jpgModuleUrl = self.getNowItemLink(self.curItem)
             self.baseImgUrl = self.basePathUrl + jpgModuleUrl[3] + '/'
@@ -84,149 +90,172 @@ class spider(threading.Thread):
             except:
                 log.error("jpgModuleUrl[-2] = {0}",jpgModuleUrl[-2])
         else:
-            self.serviceIndex += 1
-
-        if not os.path.exists('static/images/' + self.itemname + '/' + str(index)):
-            os.mkdir('static/images/' + self.itemname + '/' + str(index))
+            self.serviceIndex += 1  
+                                
+        if not os.path.exists(path + '/' + str(index)):
+            os.mkdir(path + '/' + str(index))            
         url = self.templeImgUrl + str(self.serviceIndex) + '/'
         return url
 
-    def getDownCurIndexByFile(self):
-        dirList = os.listdir('static/images/' + self.itemname)
+    def getDownCurIndexByFile(self,path):
+        dirList = os.listdir(path)
         #find last empty dir file
-        lastIndex = 0
+        lastIndex = 1
         for dirFile in dirList:
-            if len(os.listdir('static/images/' + self.itemname + '/' + dirFile)) < 10:
+            if len(os.listdir(path + '/' + dirFile)) < 3:
                 lastIndex = int(dirFile)
                 break
             else:
                 lastIndex += 1
         return lastIndex
 
-    def downItemImage(self):
+    def threadDownImage(self,args):
+        cartoonItemPath = args['path']
+        i = args['index']
+        second_catalogue = self.combinationImageUrl(i,cartoonItemPath)
+        index = 1
+        reCount = 0
+        while True and reCount < 3:
+            time.sleep(0.1)
+            imgUrl = second_catalogue + str(index) + '.' + self.jgpsuf
+            rpy = self.downImage(imgUrl,str(index),cartoonItemPath +'/' + str(i) + '/','.' + self.jgpsuf)
+            log.debug("down image {0},rpy:{1}",imgUrl,rpy)
+            if 200 != rpy:
+                if index == 1:
+                    second_catalogue = self.combinationImageUrl(i,cartoonItemPath,update = False)
+                    reCount += 1
+                    continue
+                else:
+                    break                    
+            index += 1
+        
+    def downItemImage(self,itemname):
         self.imgInfo = {}
         self.imgItemLen = len(self.imgItemLinkList)
         try:
-            if not os.path.exists('static/images/' + self.itemname):
-                os.mkdir('static/images/' + self.itemname)
+            cartoonItemPath = 'static/images/' + self.direct + '/' + itemname
+            log.debug(cartoonItemPath)
+            if not os.path.exists(cartoonItemPath):
+                os.makedirs(cartoonItemPath)  
             try:
-                lastIndex = self.updateItemNames[self.itemname]
+                lastIndex = self.updateItemNames[itemname]
+                if lastIndex == 0:
+                    lastIndex = self.getDownCurIndexByFile(cartoonItemPath)    
             except:
-                lastIndex = self.getDownCurIndexByFile()
+                lastIndex = self.getDownCurIndexByFile(cartoonItemPath)
             log.info("lasterIndex = {0},all dir file = {1}",lastIndex,self.imgItemLen)
-            for i in range(lastIndex,self.imgItemLen):
-                second_catalogue = self.combinationImageUrl(i)
-                index = 1
-                reCount = 0
-                while True and reCount < 3:
+            threadArgs = {}
+            threadArgs['path'] = cartoonItemPath         
+            with ThreadPoolExecutor(self.imgItemLen - lastIndex + 1) as t2:              
+                for i in range(lastIndex,self.imgItemLen+1):
+                    threadArgs['index'] = i
                     time.sleep(0.1)
-                    imgUrl = second_catalogue + str(index) + '.' + self.jgpsuf
-                    rpy = self.downImage(imgUrl,str(index),'images/' + self.itemname +'/' + str(i) + '/','.' + self.jgpsuf)
-                    log.debug("down image {0},rpy:{1}",imgUrl,rpy)
-                    if 200 != rpy:
-                        if index == 1:
-                            second_catalogue = self.combinationImageUrl(i,update = False)
-                            reCount += 1
-                            continue
-                        else:
-                            break
-                    
-                    index += 1
+                    t2.submit(self.threadDownImage, threadArgs)        
+
         except Exception as e:
             log.error(e.args)
             log.error(e.strerror)
             log.error(second_catalogue)
             log.error(imgUrl)
-    def run(self):
-        self.homeHtml = self.downHtml()
-        self.getWeekHref()
-        self.foreachHTML()
-        self.getItemImageInfo()
-        # self.downdpicImg()
-        # self.downtitleList()
     # down dpic file
     def downdpicImg(self):
-        for dpicSrc in self.weekdpicList:
-            index = self.weekdpicList.index(dpicSrc)
-            itemname = self.weeknameList[index]
-            self.downImage(dpicSrc,itemname,'dpic/')
+        if not os.path.exists('static/dpic/' + self.direct):
+            os.mkdir('static/dpic/' + self.direct)
+        cartoonInfo = self.carToonInfoDict[self.direct]
+        for dpicSrc in cartoonInfo['dpic']:
+            index = cartoonInfo['dpic'].index(dpicSrc)
+            itemname = cartoonInfo['name'][index]                
+            rpy = self.downImage(dpicSrc,itemname,'static/dpic/' + self.direct + '/')
+            log.debug("{0}--{1}:{2}",itemname,dpicSrc,rpy)      
     def downtitleList(self):
-        for itemurl in self.weekItemList:  
-            index = self.weekItemList.index(itemurl)
-            self.itemname = self.weeknameList[index]
-            log.debug('find name:' + self.itemname)
-            itemhtml = self.downHtml(self.homeUrl + itemurl,filename = self.itemname + ".html")
+        if not os.path.exists('static/title/' + self.direct):
+            os.mkdir('static/title/' + self.direct)
+        cartoonInfo = self.carToonInfoDict[self.direct]
+        print(cartoonInfo)
+        for itemurl in cartoonInfo['url']:
+            index = cartoonInfo['url'].index(itemurl)
+            itemname = cartoonInfo['name'][index]
+            itemhtml = self.downHtml(self.homeUrl + itemurl,filename = itemname + ".html")
             itemhtml = etree.HTML(itemhtml)
-            #get href info
             self.imgItemTitleList = itemhtml.xpath("//div[@class='stab_list']//li/a/text()")
-            self.savaTitle(self.imgItemTitleList,self.itemname)    
+            self.savaTitle(self.imgItemTitleList,itemname,path='static/title/' + self.direct + '/')
+               
     def getItemImageInfo(self):
-        for itemurl in self.weekItemList:  
-            index = self.weekItemList.index(itemurl)
-            self.itemname = self.weeknameList[index]
-            if len(sys.argv) == 2 and self.itemname != sys.argv[1]:
-                continue
+        cartoonInfo = self.carToonInfoDict[self.direct]
+        for itemurl in cartoonInfo['url']:
+            index = cartoonInfo['url'].index(itemurl)
+            itemname = cartoonInfo['name'][index]
             if len(self.updateItemNames) != 0:
                 names = self.updateItemNames.keys()
-                if self.itemname not in names:
+                if itemname not in names:
                     continue
             
-            log.debug('find name:' + self.itemname)
-            itemhtml = self.downHtml(self.homeUrl + itemurl,filename = self.itemname + ".html")
+            log.debug('find name:' + itemname)
+            itemhtml = self.downHtml(self.homeUrl + itemurl,filename = itemname + ".html")
             itemhtml = etree.HTML(itemhtml)
             #get href info
             self.imgItemLinkList = itemhtml.xpath("//div[@class='stab_list']//li/a/@href")
-            self.imgItemTitleList = itemhtml.xpath("//div[@class='stab_list']//li/a/text()")
-            self.savaTitle(self.imgItemTitleList,self.itemname)
-            self.downItemImage()
+            self.downItemImage(itemname)
      
-    def foreachHTML(self):
-        self.weekItemList = []
-        self.weeknameList = []
-        self.weekdpicList = []
-        for dayUrl in self.weekUrlList:
+    def foreachWeekHTML(self):
+        self.carToonInfoDict = {}
+        weekItemList = []
+        weeknameList = []
+        weekdpicList = []        
+        for dayUrl in self.weekUrlList:    
             self.index = self.weekUrlList.index(dayUrl)
             filename = 'week' + str(self.index + 1) + ".html"
             weekhtml = self.downHtml(self.homeUrl + dayUrl,filename = filename)
             if weekhtml == '':
                 log.error(self.homeUrl + dayUrl + " open False")
             weekhtml = etree.HTML(weekhtml)
-            weekitem = weekhtml.xpath('//div[@class="li_img"]/a/@href')
-            weekname = weekhtml.xpath('//a[@class="alink"]/@title')
-            weekdpic = weekhtml.xpath('//img[@class="dpic dh"]/@src')
-            self.weekItemList += weekitem
-            self.weeknameList += weekname
-            self.weekdpicList += weekdpic
+            weekItemList += weekhtml.xpath('//div[@class="li_img"]/a/@href')
+            weeknameList += weekhtml.xpath('//a[@class="alink"]/@title')
+            weekdpicList += weekhtml.xpath('//img[@class="dpic dh"]/@src')
+        self.carToonInfoDict['week'] = {'url':weekItemList,'name':weeknameList,'dpic':weekdpicList}
+    def foreachYearHTML(self):
+        for yearUrl in self.yearUrlList:
+            self.index = self.yearUrlList.index(yearUrl)            
+            downUrl = yearUrl
+            curIndex = 1
+            yearItemList = []
+            yearnameList = []
+            yeardpicList = []
+            while True:
+                filename = 'year_' + self.yearNameList[self.index] + '-' +str(curIndex) + ".html"
+                yearhtml = self.downHtml(self.homeUrl + downUrl,filename = filename)
+                if yearhtml == '':
+                    log.error(self.homeUrl + downUrl + " open False")
+                    break
+                yearhtml = etree.HTML(yearhtml)
+                yearItemList += yearhtml.xpath('//div[@class="li_img"]/a/@href')
+                yearnameList += yearhtml.xpath('//a[@class="alink"]/@title')
+                yeardpicList += yearhtml.xpath('//img[@class="dpic dh"]/@src')
+                downUrl = yearhtml.xpath('//a/@href')[-3]
+                downText = yearhtml.xpath('//a/text()')[-2]
+                downUrl = downUrl[1:]
+                if downText != r'下页':
+                    self.carToonInfoDict[self.yearNameList[self.index]] = {'url':yearItemList,'name':yearnameList,'dpic':yeardpicList} 
+                    break
+                curIndex += 1
         pass
     def getWeekHref(self):
         self.homeHtml = etree.HTML(self.homeHtml)
         self.weekUrlList = self.homeHtml.xpath('//div[@class="nav_down clearfix"]/div[@class="nav_1000"]//li/a/@href')[:7]
         self.yearUrlList = self.homeHtml.xpath('//div[@class="nav_down clearfix"]/div[@class="nav_1000"]//li/a/@href')[7:]
-if __name__ == '__main__':
-    homeURL= 'https://www.wshm23.com/'
-    thread1 = spider(homeURL,update = False,itemNames = {'姐姐爱做菜':14},isSaveHtml=False)
-    thread1.start()
-    thread1.join()
-    log.info("退出主线程")
-    # thread1 = spider(homeURL,0)
-    # thread2 = spider(homeURL,1)
-    # thread3 = spider(homeURL,2)
-    # thread4 = spider(homeURL,3)
-    # thread5 = spider(homeURL,4)
-    # thread6 = spider(homeURL,5)
-    # thread7 = spider(homeURL,6)
-    # # 开启新线程
-    # thread1.start()
-    # thread2.start()
-    # thread3.start()
-    # thread4.start()
-    # thread5.start()
-    # thread6.start()
-    # thread7.start()
-    # thread1.join()
-    # thread2.join()
-    # thread3.join()
-    # thread4.join()
-    # thread5.join()
-    # thread6.join()
-    # thread7.join()
+        self.yearNameList = self.homeHtml.xpath('//div[@class="nav_down clearfix"]/div[@class="nav_1000"]//li/a/text()')[7:]
+    def run(self):
+        if self.cmd == 0:
+            self.getItemImageInfo()
+        if self.cmd == 1:
+            self.downdpicImg()
+        if self.cmd == 2:
+            self.downtitleList()
+        
+        log.info("update&down finish")
+# if __name__ == '__main__':
+#     homeURL= 'https://www.wshm23.com/'
+#     thread1 = spider(homeURL,update = False,itemNames = {'超级公务员':30},isSaveHtml=False)
+#     thread1.start()
+#     thread1.join()
+#     log.info("退出主线程")
