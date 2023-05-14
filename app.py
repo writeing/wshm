17a3cbb69598ccaa36252e18f470960e174b8ecf
writeing_home <xuanchaowang@qq.com>
2023-05-14 23:00:42 +0800

    complete down and show

---
+++ app.py
@@ -0,0 +1,227 @@
+from flask import Flask,render_template,redirect,request
+from flask import url_for
+from markupsafe import escape
+import os
+import re
+from spiderFour import spider
+import threading
+from loguru import logger as log
+app = Flask(__name__)
+
+
+movies = {}
+
+
+def readLocalFile(direct = ''):
+    global movies
+    movies = {}
+    titlePath = 'static/title/'
+    try:
+        titleFileNames = os.listdir(titlePath)
+    except:
+        downLoadcmd(2,direct)
+        titleFileNames = os.listdir(titlePath)
+    dpicPath = 'static/dpic/'
+    try:
+        dpicFileNames = os.listdir(dpicPath)
+    except:
+        downLoadcmd(1,direct)
+        dpicFileNames = os.listdir(dpicPath)                
+    for names in dpicFileNames:
+        name = re.split('\.',names)[0]
+        movies[name] = names
+sg_items = []
+def readLocalTitle(name):
+    global sg_items
+    titlePath = 'static/title/'
+    titleFileNames = os.listdir(titlePath)
+    for tt in titleFileNames:
+        if name in tt:        
+            with open('static/title/' + tt,'r') as file:
+                sg_items = file.readlines()
+def getFileName(item):
+    dpicPath = 'static/dpic/'
+    dpicFileNames = os.listdir(dpicPath)
+    for tt in dpicFileNames:
+      if item in tt:        
+            return tt.split('.')[0]
+def sort_key(s):
+    # 排序关键字匹配
+    # 匹配开头数字序号
+    if s:
+        try:
+            c = re.findall('^\d+', s)[0]
+        except:
+            c = -1
+        return int(c)
+
+def strsort(alist):
+    alist.sort(key=sort_key)
+    return alist
+
+imgList = []
+def readLocalImg(name,itemName):
+    global imgList
+    imagesPath = 'static/images/'
+    imagesFileNames = os.listdir(imagesPath)
+    for tt in imagesFileNames:
+        if name in tt:
+            path = 'static/images/' + tt + "/" + itemName
+            imgList = strsort(os.listdir(path))
+            break
+    print(imgList)
+
+homeURL= 'https://www.wshm.cc/'
+
+downLoadItemDict = {"":""}
+def downLoadItem(item,index = 1):
+    global downLoadItemDict
+    log.info("begin down file {0}",item)
+    item = getFileName(item)
+    thread1 = spider(homeURL,update = False,itemNames = {"name":item,"index":index},isSaveHtml=False,cmd=2)
+    thread1.start()
+    thread1.join(1)
+    downLoadItemDict[item] = thread1
+    
+def downLoadcmd(cmd,direct='week'):
+    thread1 = spider(homeURL,update = False,isSaveHtml=False,cmd=cmd)
+    thread1.start()
+    thread1.join(1)
+    
+globalUpdate = False
+def downLoadInit():
+    global globalUpdate
+    thread1 = spider(homeURL,update = globalUpdate,isSaveHtml=True,cmd = 3)
+    thread1.start()
+    thread1.join(1)
+    return thread1.yearNameList
+
+
+historyList = []
+
+def buttonExec(request):
+    global historyList,downLoadItemDict
+    if request.method == 'POST': 
+        name = list(request.values.keys())[0]
+        bt_name = request.values.get(name)
+        for year in historyList:
+            if bt_name == year:
+                print(bt_name)
+                return year
+        
+        if (bt_name == '更新dpic'):
+            downLoadcmd(1,'week')
+        elif (bt_name == '更新title'):
+            downLoadcmd(2,'week')
+        elif (bt_name == '更新主页面'):
+            downLoadInit()
+        else:
+            try:
+                print(bt_name)
+                print(downLoadItemDict)
+                downLoadItemDict[bt_name].cancel()
+            except:
+                print("had cancel")
+    return 'week'
+@app.route('/',methods=['POST', 'GET'])
+def rootHome():
+    global historyList,downLoadItemDict
+    # if len(historyList) == 0:
+    #     historyList = downLoadInit()
+    readLocalFile('')
+    return render_template('index.html',name = 'four',movies=movies,historyList = historyList,downItemDict = downLoadItemDict )
+
+@app.route('/user/<name>')
+def user_page(name):
+    return f'User: {escape(name)}'
+
+@app.route('/wshm/up/<name>/<itemName>')
+def page_up(name,itemName):
+    if len(sg_items) == 0:
+        readLocalTitle(name)    
+    proitem = ' '
+    for item in sg_items:        
+        if itemName in item:         
+           proitem = item   
+           break
+    if proitem != ' ' :
+        index = sg_items.index(proitem)
+        proitem = sg_items[index + 1].split('\n')[0] 
+        readLocalImg(name,proitem)
+    else:
+        item = getFileName(name)
+        ss = sg_items[::-1]
+        return render_template('item.html',name = 'wshm',allItems = ss,item = item)
+    log.debug(proitem)       
+    return render_template('showImg.html',name = name,itemName = proitem,imgList=imgList)
+@app.route('/wshm/page/<item>')
+def page_page(item):
+    if len(sg_items) == 0:
+        readLocalTitle(item)
+    item = getFileName(item)
+    ss = sg_items[::-1]
+    return render_template('item.html',name = 'wshm',allItems = ss,item = item)
+
+@app.route('/wshm/down/<name>/<itemName>')
+def page_down(name,itemName):
+    if len(sg_items) == 0:
+        readLocalTitle(name)
+    proitem = sg_items[0]
+    for item in sg_items:        
+        if itemName in item:
+            proitem = proitem.split('\n')[0]         
+            readLocalImg(name,proitem)
+            break
+        proitem = item
+    print(imgList)
+    return render_template('showImg.html',name = name,itemName = proitem,imgList=imgList)
+
+
+
+@app.route('/wshm/<item>',methods=['POST', 'GET'])
+def wshmItem(item):
+    item = item.split('.')[0]
+    readLocalTitle(item)
+    index = 1
+    if request.method == 'POST': 
+        name = list(request.values.keys())[0]
+        log.debug("name:{0}",request.values.keys())
+        bt_name = request.values.get(name)
+        log.debug("btn:{0}",bt_name)
+        if 'allupdate' in request.values.keys():
+            downLoadItem(item)
+        # if 'singupdate' in request.values.keys():
+        #     index = request.form.get('index')
+        #     downLoadItem(item,int(index))
+            
+    log.info("index:{0},item:{1}",index,item)
+    ss = sg_items[::-1]
+    return render_template('item.html',name = 'wshm',allItems = ss,item = item)
+
+@app.route('/wshm/img/<item>/<num>/<itemName>')
+def wshmImgShow(item,num,itemName):
+    readLocalImg(item,itemName)
+    return render_template('showImg.html',name = item,num = num,imgList=imgList,itemName=itemName)
+
+
+@app.route('/wshm/main')
+def wshm_main():
+    return redirect(url_for(test_url_for))
+
+@app.route('/test')
+def test_url_for():
+    # 下面是一些调用示例（请访问 http://localhost:5000/test 后在命令行窗口查看输出的 URL）：
+    print(url_for('rootHome'))  # 生成 hello 视图函数对应的 URL，将会输出：/
+    # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
+    print(url_for('user_page', name='greyli'))  # 输出：/user/greyli
+    print(url_for('user_page', name='peter'))  # 输出：/user/peter
+    print(url_for('test_url_for'))  # 输出：/test
+    # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
+    print(url_for('test_url_for', num=2))  # 输出：/test?num=2
+    return 'Test page'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    globalUpdate = False
+
+
---
+++ spiderFour.py
@@ -0,0 +1,270 @@
+import requests
+from lxml import etree
+import os
+import re
+import time
+from loguru import logger as log
+import threading
+import sys
+from concurrent.futures import ThreadPoolExecutor
+from datetime import date,timedelta,datetime
+import json
+class spider(threading.Thread):
+    homeUrl = 'https://www.wshm.cc/'
+    homeHtml = ''
+    index = 0
+    basePathUrl = 'https://img.pic-server.com/'
+    serviceIndex = 0
+    today = date.today()    
+    nowDate = today.strftime("_%d_%m_%Y")
+    jsonRpy = {"item":[]}
+    def __init__(self,fileUrl,update= False,itemNames={},isSaveHtml = False,cmd=0,catalogue='week'):
+        threading.Thread.__init__(self)
+        self.homeUrl = fileUrl
+        self.update = update
+        self.updateItemNames = itemNames
+        self.isSaveHtml = isSaveHtml
+        self.cmd = cmd
+        self.catalogue = catalogue
+        log.debug(self.nowDate)
+
+    def downImage(self,url,name,path,imgType = '.jpg'):
+        if os.path.exists( path + name + imgType):
+            log.info('image:{0} had down \n',name)
+            return 200
+        else:
+            if not os.path.exists(path):
+                os.makedirs(path)
+        img = requests.get(url)
+        if img.status_code != 200:
+            return img.status_code
+        try:
+            with open(path +  name + imgType,'wb') as file:
+                file.write(img.content)
+        except:
+            log.error("write file error {0} {1}",path +  name + imgType,type(img.content))
+        return img.status_code
+    
+    def savaTitle(self,listTitle,name,path = 'static/title/'):        
+        with open(path+name+'.ini','w') as file:
+            for title in listTitle:
+                file.write(title + "\n")
+    def savaUrl(self,listTitle,name,path = 'static/title/'):        
+        with open(path+name+'.json','w') as file:
+                file.write(listTitle + "\n")
+    def saveImage(self,threadArgs):
+        index = threadArgs['index']
+        path = threadArgs['path']
+        title = threadArgs['title']
+        imagesurl = "https://cdn.ifs7gsd2f.com/" + threadArgs['imageslist'][index]
+        self.downImage(imagesurl,str(index),path)
+        log.debug("{0}--{1}:{2}",title,index,len(threadArgs['imageslist']))    
+    def threadDownImage(self,args):
+        index = args['index']
+        cartoonInfo = args['dictJson'][index]
+        if not os.path.exists('static/images/' + cartoonInfo['title']):
+            os.mkdir('static/images/' + cartoonInfo['title'])
+        itemJson = ''
+        with open('static/url/' + cartoonInfo['title'] + '.json','r') as file:
+            itemJson = json.loads(file.read())
+        # log.warning()("begin down item--{0}:{1}",cartoonInfo['title'],len(itemJson['result']['list']))
+        for curlist in itemJson['result']['list']:
+            title = curlist['title']
+            imageslist = curlist['imagelist'].split(',')
+            log.debug("begin down images :{0}:{1}/{2}",cartoonInfo['title'],title,len(imageslist))
+            
+            threadArgs = {}
+            threadArgs['imageslist'] = imageslist
+            threadArgs['title'] = title
+            threadArgs['path'] = 'static/images/' + cartoonInfo['title'] + "/" + title + '/'
+            with ThreadPoolExecutor(len(imageslist)) as t2:    
+                for j in range(0,len(imageslist)):
+                    threadArgs['index'] = j
+                    t2.submit(self.saveImage, threadArgs)
+            # i = 0
+            # for image in imageslist:
+            #     print(image)
+            #     self.downImage("https://cdn.ifs7gsd2f.com/" + image,str(i),'static/images/' + cartoonInfo['title'] + "/" + title + '/')
+            #     i += 1
+        self.ImageDownCount += 1
+        log.debug("{0}--{1}:{2}",index,cartoonInfo['title'],self.ImageDownCount)                       
+        
+    def downItemImage(self):
+        if len(self.jsonRpy['item']) == 0:
+            log.warning("not load json data")
+            self.loadSaveJson()  
+        try:
+            threadArgs = {}
+            threadArgs['dictJson'] = self.jsonRpy['item']
+            self.ImageDownCount = 0
+            maxThread = 20
+            for i in range(0,len(self.jsonRpy['item']),maxThread):
+                log.debug("begin down images {0}",i)
+                with ThreadPoolExecutor(maxThread) as t2:    
+                    for j in range(i,i + maxThread):
+                        threadArgs['index'] = j
+                        t2.submit(self.threadDownImage, threadArgs)
+                while True:
+                    if self.ImageDownCount == i + maxThread or self.ImageDownCount == len(self.jsonRpy['item']):
+                        log.debug("had down a cycle:{0}",self.ImageDownCount)
+                        break
+        except Exception as e:
+            log.error(e.args)
+            log.error(e.strerror)
+            log.error(second_catalogue)
+            log.error(imgUrl)
+    
+    def downSignalImage(self,threadArgs):
+        imagesInfo = threadArgs['imagedict']
+        imageslist = imagesInfo['imagelist'].split(',')
+        i = 0
+        title = imagesInfo['title']
+        itemName = self.updateItemNames['name']
+        for image in imageslist:
+            self.downImage("https://cdn.ifs7gsd2f.com/" + image,str(i),'static/images/' + itemName + "/" + title + '/')
+            log.debug("{0}--{1}---{2}/{3}",itemName,title,i,len(imageslist))
+            i += 1
+        pass
+
+    def downSignalItemImage(self):
+        if len(self.jsonRpy['item']) == 0:
+            log.warning("not load json data")
+            self.loadSaveJson()                  
+        threadArgs = {}
+        itemJson = ''
+        itemName = self.updateItemNames['name']
+        self.itemDownCount = 0
+        for item in self.jsonRpy['item']:
+            if itemName == item['title']:
+                threadArgs['dictJson'] = item
+                self.threadDownUrl(threadArgs)
+        with open('static/url/' +  threadArgs['dictJson']['title'] + '.json','r') as file:
+            itemJson = json.loads(file.read())['result']
+        maxThread = int(itemJson['totalRow'])
+    
+        with ThreadPoolExecutor(maxThread) as t2:    
+            for j in range(0,maxThread):
+                threadArgs['imagedict'] = itemJson['list'][j]
+                t2.submit(self.downSignalImage, threadArgs)
+                
+    def threadDownDpic(self,threadArgs):
+        index = threadArgs['index']        
+        cartoonInfo = threadArgs['dictJson'][index]
+        # if not os.path.exists('static/dpic/' + cartoonInfo['title']):
+        #     os.mkdir('static/dpic/' + cartoonInfo['title'])
+        dpicSrc = "https://cdn.ifs7gsd2f.com" + cartoonInfo['image']
+        rpy = self.downImage(dpicSrc,cartoonInfo['title'],'static/dpic/')
+        self.dhcpDownCount += 1
+        log.debug("{0}--{1}:{2}:{3}",index,cartoonInfo['title'],rpy,self.dhcpDownCount)
+        pass
+    def loadSaveJson(self):
+        with open('static/item.json','r') as file:
+            rpy = file.read()
+            self.jsonRpy = json.loads(rpy)
+    # down dpic file
+    def downdpicImg(self):
+        if len(self.jsonRpy['item']) == 0:
+            log.warning("not load json data")
+            self.loadSaveJson()            
+        threadArgs = {}
+        threadArgs['dictJson'] = self.jsonRpy['item']
+        self.dhcpDownCount = 0
+        for i in range(0,len(self.jsonRpy['item']),100):
+            log.debug("begin down dpic {0}",i)
+            with ThreadPoolExecutor(100) as t2:       
+                for j in range(i,i + 100):
+                    threadArgs['index'] = j
+                    t2.submit(self.threadDownDpic, threadArgs)
+            while True:
+                if self.dhcpDownCount == i + 100 or self.dhcpDownCount == len(self.jsonRpy['item']):
+                    log.debug("had down a cycle:{0}",self.dhcpDownCount)
+                    break
+    def saveTitleForJson(self,jsondata,title):
+        rpy = json.loads(jsondata)
+        listTitle = []
+        for i in rpy['result']['list']:
+            listTitle.append(i['title'])
+        self.savaTitle(listTitle,title)
+    def threadDownUrl(self,threadArgs):
+    
+        cartoonInfo = threadArgs['dictJson']
+
+        dpicSrc = "https://www.a8b77.com/home/api/chapter_list/tp/{0}-0-1-{1}".format(cartoonInfo['id'],'1')
+        rpy = requests.get(dpicSrc).text
+        
+        itemLen = json.loads(rpy)['result']['totalRow']
+        
+        dpicSrc = "https://www.a8b77.com/home/api/chapter_list/tp/{0}-0-1-{1}".format(cartoonInfo['id'],str(itemLen))
+        rpy = requests.get(dpicSrc).text
+        self.saveTitleForJson(rpy,cartoonInfo['title'])
+        self.savaUrl(rpy,cartoonInfo['title'],'static/url/')        
+        self.itemDownCount += 1
+        log.debug("{0}:{1}",cartoonInfo['title'],self.itemDownCount)
+        pass
+# down url
+    def downUrl(self):
+        if len(self.jsonRpy['item']) == 0:
+            log.warning("not load json data")
+            self.loadSaveJson()          
+        threadArgs = {}
+        self.itemDownCount = 0
+        for i in range(0,len(self.jsonRpy['item']),100):
+            log.debug("begin down url {0}",i)
+            with ThreadPoolExecutor(100) as t2:       
+                for j in range(i,i + 100):
+                    threadArgs['dictJson'] = self.jsonRpy['item'][j]
+                    t2.submit(self.threadDownUrl, threadArgs)
+            while True:
+                if self.itemDownCount == i + 100 or self.itemDownCount == len(self.jsonRpy['item']):
+                    log.debug("had down a cycle:{0}",self.itemDownCount)
+                    break
+    def getAllItemJson(self):
+        itemCurIndex = 1
+        originHttp = 'https://www.a8b77.com/home/api/cate/tp/1-0-2-1-{0}'
+        
+        while True:
+            nowhttp = originHttp.format(str(itemCurIndex))
+            itemCurIndex += 1            
+            log.debug(nowhttp)
+            rpy = requests.get(nowhttp).text
+            rpy = json.loads(rpy)    
+            self.jsonRpy['item'] += rpy['result']['list']
+            log.info(rpy['result']['lastPage'])
+            if rpy['result']['lastPage'] == True:
+                break
+        log.info(len(self.jsonRpy['item']))
+        with open('static/item.json','w') as ff:
+            ff.write(json.dumps(self.jsonRpy))
+        log.info("had down save item json :{0}",len(self.jsonRpy['item']))
+            
+
+    def run(self):
+        if self.cmd == 0:
+            self.getAllItemJson()
+        if self.cmd == 1:
+            self.downdpicImg()
+        if self.cmd == 2:
+            if len(self.updateItemNames) == 0:
+                self.downItemImage()
+            else:
+                self.downSignalItemImage()
+        if self.cmd == 3:
+            self.downUrl()
+        log.info("update&down finish")
+
+# with open("temp/home_14_05_2023.html",'r') as file:
+#     rpy = file.read()
+#     rpy = json.loads(rpy)
+#     print(len(rpy['result']['list']))
+#     print(rpy['result']['list'][0])
+    # print(len(rpy['result']['list']))
+    # print(rpy['result']['list'][0])
+
+# https://www.a8b77.com/home/api/chapter_list/tp/1251-0-1-30
+# if __name__ == '__main__':
+#     # homeURL= 'https://www.a8b77.com/home/api/cate/tp/1-0-2-1-2'
+#     homeURL= 'https://www.a8b77.com/home/api/chapter_list/tp/1251-0-1-30'
+#     thread1 = spider(homeURL,update = True,itemNames = {"name":'小巷裡的秘密 ',"index":10},isSaveHtml = True,cmd=2)
+#     thread1.start()
+#     thread1.join()
+#     log.info("退出主线程")
